2de8f5a58b947cca524ff6af3127b57e254651a4
Rikard <rikardengstrom89@gmail.com>
2025-08-14 13:22:36 +0200

    first

---
+++ src/PetrolPurchase.java
@@ -0,0 +1,56 @@
+public class PetrolPurchase {
+
+    private String location;
+    private String petrol;
+    private int quantityInLitres;
+    private double pricePerLitre;
+    private double discount;
+
+    public PetrolPurchase(String location, String petrol, int quantityInLitres, double pricePerLitre, double discount) {
+        this.location = location;
+        this.petrol = petrol;
+        this.quantityInLitres = quantityInLitres;
+        this.pricePerLitre = pricePerLitre;
+        this.discount = discount;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getPetrol() {
+        return petrol;
+    }
+
+    public void setPetrol(String petrol) {
+        this.petrol = petrol;
+    }
+
+    public int getQuantityInLitres() {
+        return quantityInLitres;
+    }
+
+    public void setQuantityInLitres(int quantityInLitres) {
+        this.quantityInLitres = quantityInLitres;
+    }
+
+    public double getPricePerLitre() {
+        return pricePerLitre;
+    }
+
+    public void setPricePerLitre(double pricePerLitre) {
+        this.pricePerLitre = pricePerLitre;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+}
